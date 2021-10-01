76b4c7cc7ec42033d7771a4310ac5e40527f8fb7
vagrant <vagrant@homestead>
2021-10-01 07:20:23 +0000

    基础页面结构

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,7 +3,9 @@
     <p class="float-left">
       由 <a href="#">小信</a>&nbsp;设计和编码
       <span style="color: #e27575;font-size: 14px">❤</span>
-      <a href="#" class="float-end">联系我们</a>
+    </p>
+    <p class="float-right">
+      <a href="#">联系我们</a>
     </p>
   </div>
 </footer>
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -19,12 +19,37 @@
       </ul>
       {{--      右导航--}}
       <ul class="navbar-nav navbar-right">
-        <li class="nav-item">
-          <a href="#" class="nav-link">登录</a>
-        </li>
-        <li class="nav-item">
-          <a href="#" class="nav-link">注册</a>
-        </li>
+        {{--        判断是否已登录--}}
+        @guest
+          <li class="nav-item">
+            <a href="{{route('login')}}" class="nav-link">登录</a>
+          </li>
+          <li class="nav-item">
+            <a href="{{route('register')}}" class="nav-link">注册</a>
+          </li>
+        @else
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle"
+               id="navbarDropdown" role="button" data-toggle="dropdown"
+               aria-haspopup="true" aria-expanded="false"
+            >
+              <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60"
+                   class="img-responsive img-circle" width="30px" height="30px" alt="">
+              {{\Illuminate\Support\Facades\Auth::user()->name}}
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a href="" class="dropdown-item">个人中心</a>
+              <a href="" class="dropdown-item">编辑资料</a>
+              <div class="dropdown-divider"></div>
+              <a href="#" class="dropdown-item" id="logout">
+                <form action="{{route('logout')}}" method="post">
+                  {{csrf_field()}}
+                  <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                </form>
+              </a>
+            </div>
+          </li>
+        @endguest
       </ul>
     </div>
   </div>

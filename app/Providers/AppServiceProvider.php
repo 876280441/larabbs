df79256ce2fa6e374aa779e3e36cf75fc635634c
vagrant <vagrant@homestead>
2021-10-04 10:25:37 +0000

    生成话题骨架

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,10 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
+	{
+		\App\Models\User::observe(\App\Observers\UserObserver::class);
+		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+
 
     }
 }

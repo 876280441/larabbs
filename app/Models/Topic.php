e4e6203aa35e340e05042780e2195f38c8a7e402
vagrant <vagrant@homestead>
2021-10-12 04:00:05 +0000

    删除评论

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -67,4 +67,11 @@ class Topic extends Model
     {
         return $this->hasMany(Reply::class);
     }
+    /*
+     * 更新评论数
+     */
+    public function updateReplyCount(){
+        $this->reply_count = $this->replies->count();
+        $this->save();
+    }
 }
